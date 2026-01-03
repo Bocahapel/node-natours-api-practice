b6ddb3973fe989e345b8a0f17ff3567a3befa640
Bocahapel <pratamadrian.putra@gmail.com>
2026-01-03 21:32:35 +0700

    Update app.js
    Add get and post routing
    Testing post routing

--- app.js
+++ app.js
@@ -1,6 +1,61 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+
+app.use(express.json());
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello World', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(200).send('Post here');
+// });
+
+//===========
+//read data from file
+//===========
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+//===========
+//Get endpoint
+//===========
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    message: 'success',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+//===========
+//Post endpoint
+//===========
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'Success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+
+  // res.send('Done');
+});
 
 const port = 3000;
 
