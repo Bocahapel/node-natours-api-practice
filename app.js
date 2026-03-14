88591f7a1d86690211c28af1cea73e00b52b6b87
Bocahapel <pratamadrian.putra@gmail.com>
2026-03-14 23:16:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 const port = 3000;
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
+);
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from server side');
+//get
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'Success',
+    data: { tours },
+  });
 });
 
 app.listen(port, () => {
