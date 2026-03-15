a784b294c138d003d7bc1d1473f7c4811b679b89
Bocahapel <pratamadrian.putra@gmail.com>
2026-03-15 11:53:17 +0700

    update app.js
    adding post endpoint

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
-const port = 3000;
+
+app.use(express.json()); //middleware
+const port = 3000; //port
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
-);
+); //to read from file into json
 
 //get
 app.get('/api/v1/tours', (req, res) => {
@@ -13,6 +15,23 @@ app.get('/api/v1/tours', (req, res) => {
     status: 'Success',
     data: { tours },
   });
+});
+
+//post
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    },
+  );
+  res.send('Done');
 });
 
 app.listen(port, () => {
