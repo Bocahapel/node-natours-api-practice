ae1a5e5b26425f446d17d4fd82b11cb4fc2cb30d
Bocahapel <pratamadrian.putra@gmail.com>
2026-05-25 17:37:01 +0700

    add db connection using mongoose

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 const app = require('./app');
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connection);
+    console.log('DB connection success');
+  });
 
 console.log(process.env);
 const port = process.env.PORT || 3000; //port
