1e93c6475b43a7307da05ee60af53e3ef6dbadca
Bocahapel <pratamadrian.putra@gmail.com>
2026-06-04 17:54:48 +0700

    Update tourController.js

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -32,11 +32,17 @@ const Tour = require('../models/tourModel');
 
 exports.getAll = async (req, res) => {
   try {
+    //BUILD QUERY
     console.log(req.query);
     const queryObj = { ...req.query };
     const excludeFields = ['page', 'sort', 'limit', 'fields'];
     excludeFields.forEach((el) => delete queryObj[el]);
-    const tours = await Tour.find(queryObj);
+    const query = Tour.find(queryObj);
+
+    //EXECUTE QUERY
+    const tours = await query;
+
+    //SEND RESPONSE
     res.status(200).json({
       status: 'Success',
       data: {
